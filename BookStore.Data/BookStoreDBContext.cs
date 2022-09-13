3a4ef24c68bd040439aa774f8c609b60be826a38
Quang Duy <justinquangduy@gmail.com>
2022-09-14 06:15:37 +0700

    Add project files.

---
+++ BookStore.Data/BookStoreDBContext.cs
@@ -0,0 +1,18 @@
+﻿using BookStore.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore.Data
+{
+    public class BookStoreDBContext : DbContext
+    {
+        public BookStoreDBContext(DbContextOptions<BookStoreDBContext> context) : base(context)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ BookStore.Web/Controllers/BookController.cs
@@ -0,0 +1,40 @@
+﻿using BookStore.Data.Entities;
+using BookStore.Data.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Web.Controllers
+{
+    public class BookController : Controller
+    {
+        private IBookRepository _bookRepository;
+
+        public BookController(IBookRepository bookRepository) 
+        {
+            _bookRepository = bookRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var listBooks = _bookRepository.GetAllBooks();
+            return View(listBooks);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {
+            _bookRepository.AddBook(book);
+            return RedirectToAction("Index");
+        }
+
+        [Route("book/id")]
+        public IActionResult Details(int id)
+        {
+            var book = _bookRepository.GetBookDetails(id);
+            return View(book);
+        }
+    }
+}
